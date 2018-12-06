23c1d033815c1ef29f64e40d05df617e7967a17f
marioaburto <mario.aburto@cfins.com>
2018-12-06 10:39:20 -0500

    added click functionality and an if case statement

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,19 +8,27 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  hero: Hero = {
-    id: 1,
-    name: 'Spiderman'
-    // strongPoints: 450
-  };
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
+  }
 
-  heroes = HEROES;
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Spiderman'
+  //   // strongPoints: 450
+  // };
+
+  
 
 } 
  
