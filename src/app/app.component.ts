61e117ea7d74ccb3d32d70f82a5a255ab27b65f9
marioaburto <mario.aburto@cfins.com>
2018-12-06 13:54:50 -0500

    added routing functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My New Angular App';
+  title = 'Hero Angular App';
 }
   
